82d5f6971277fd0bc2ff9a0decaf44f8c6f6ddfc
Daler <babybenzopila@gmail.com>
2022-11-14 12:02:10 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// const test = +prompt('How old are you?');
+// console.log(test + 10);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Как ваше отчество?', '');
+
+console.log(answers);
